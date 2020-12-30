ec69bee584d662ace91459c7ea39e27660cf81a9
jongseop-park <jongseop94@adcapsule.co.kr>
2020-12-30 09:21:38 +0900

    Git 생성

---
+++ src/main/java/com/sck/apps/test/controller/TestController.java
@@ -0,0 +1,21 @@
+package com.sck.apps.test.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TestController {
+
+    @RequestMapping("/")
+    public String test(){
+
+        return "main";
+    }
+
+    @RequestMapping("/detail")
+    public String index(){
+
+        return "detail";
+    }
+
+}
